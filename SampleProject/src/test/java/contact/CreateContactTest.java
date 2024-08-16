62e2a455d6f36ae39fd34fd854f59b85c612c51e
Kuilo562 <atulsharma99856@gmail.com>
2024-08-16 17:21:55 +0530

    parameter

--- SampleProject/src/test/java/contact/CreateContactTest.java
+++ SampleProject/src/test/java/contact/CreateContactTest.java
@@ -5,6 +5,10 @@ import org.testng.annotations.Test;
 public class CreateContactTest {
 	@Test
 	public void createContactTest() {
+		String URL = System.getProperty("Url");
+		String UserName=System.getProperty("username");
+		String PWD=System.getProperty("pwd");
+		String Browser=System.getProperty("browser");
 		System.out.println("createcontactTest executed");
 	}
 	@Test
