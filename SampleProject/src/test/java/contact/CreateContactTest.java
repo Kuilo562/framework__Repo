c24062fb8eee4098611d5c8cea3df16b4a23d100
Kuilo562 <atulsharma99856@gmail.com>
2024-08-16 16:34:45 +0530

    m

---
+++ SampleProject/src/test/java/contact/CreateContactTest.java
@@ -0,0 +1,16 @@
+package contact;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+	@Test
+	public void createContactTest() {
+		System.out.println("createcontactTest executed");
+	}
+	@Test
+	public void modifycontactTest() {
+		System.out.println("modifycontactTest executed");
+		//chnge
+	}
+	}
+
---
+++ SampleProject/src/test/java/organisation/CreateOrgTest.java
@@ -0,0 +1,17 @@
+package organisation;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+	@Test
+	public void createOrgTest() {
+		System.out.println("createOrgTest executed");
+	}
+	@Test
+	public void modifyOrgTest() {
+		System.out.println("modifyOrgTest executed");
+		
+	//chnges
+	}
+	}
+
